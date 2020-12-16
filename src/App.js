4a89c7e8c1fcb6a358ee67de8005596983846f26
QU35TMJ <maureenjune16@gmail.com>
2020-12-16 03:17:02 +0300

    Fetched data using random API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+
 
 function App() {
+
+  //declare state variables
+  const [categories, setCategories] = useState([]);
+  const [category, setCategory] = useState('random');
+  const [joke, setJoke] = useState('');
+  
+  //fetch category data
+  useEffect(() => {
+    fetch("https://api.chucknorris.io/jokes/categories")
+      .then(res => res.json())
+      .then(data => setCategories(data))
+      .catch(e => console.log(e));
+  }, []);
+
+  const GenerateJoke = () => {
+    let url;
+    if (category == "random") {
+      url = "https://api.chucknorris.io/jokes/random";
+    } else {
+      url = `https://api.chucknorris.io/jokes/random?category=${category}`;
+    }
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        setJoke(data.value);
+      })
+      .catch(e => console.log(e));
+  };
+
+  const selectCategory = e => {
+    setCategory(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class='app'>
+      <h1>Chuck Norris Jokes Generator</h1>
+      <span>Select Category</span>:<select name="categories" id="categories" onChange={selectCategory}>
+            <option value="random" selected>
+              random
+            </option>
+            {categories.map(category => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+          
+      <p>{joke}</p>
+      <button onClick={GenerateJoke}>New Joke</button>
+
     </div>
   );
 }
